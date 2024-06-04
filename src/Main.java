11f9c47d2f69204fd44b719f5ad02c96e17ca3a8
Sophy <phsophy2@gmail.com>
2024-06-05 00:24:57 +0700

    add new file

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import  controller.UserController;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import view.View;
+
+import javax.swing.plaf.ViewportUI;
+public class Main {
+    public static void main(String[] args){
+        View.UI();
+    }
+}
